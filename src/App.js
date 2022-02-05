3e7d83d11e2fe9fb4abe15cc4afa3c4563aecefa
German <german.a.almonte@gmail.com>
2022-02-05 12:42:48 -0500

    Basic Working music library done

--- src/App.js
+++ src/App.js
@@ -7,21 +7,31 @@ function App(){
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
 
+    const API_URL = 'https://itunes.apple.com/search?term='
+
     useEffect(() => {
         const fetchData = async () => {
             document.title = `${search} Music`
-            const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+            const response = await fetch(API_URL + search)
             const resData = await response.json()
-            console.log(resData)
+            if (resData.results.length > 0) {
+                setData(resData.results)
+            } else {
+                setMessage('Not Found')
+            }
         }
         fetchData()
-    })
+    }, [search])
 
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        setSearch(term)
+    }
     return (
         <div>
-            <Searchbar />
+            <Searchbar handleSearch = {handleSearch}/>
             {message}
-            <Gallery />
+            <Gallery data={data}/>
         </div>
     )
 }
