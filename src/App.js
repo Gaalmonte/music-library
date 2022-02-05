c184d5be0e2a27b77754ef955038da1821b4c3f5
German <german.a.almonte@gmail.com>
2022-02-05 13:03:27 -0500

    established datacontext, working on searchcontext now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
-import Searchbar from './components/Searchbar'
+import SearchBar from './components/Searchbar'
+import { DataContext } from './context/DataContext'
 
 function App(){
     let [search, setSearch] = useState('')
@@ -29,9 +30,11 @@ function App(){
     }
     return (
         <div>
-            <Searchbar handleSearch = {handleSearch}/>
+            <SearchBar handleSearch = {handleSearch}/>
             {message}
-            <Gallery data={data}/>
+            <DataContext.Provider value={data}>
+            <Gallery />
+            </DataContext.Provider>
         </div>
     )
 }
